713377f2913c78eb61a7f2ce144aaa2491695ac9
Mohamed Leila <mohamed.laila@mail.mcgill.ca>
2021-01-04 15:11:45 -0500

    [NEW] add setup file

---
+++ setup.py
@@ -0,0 +1,6 @@
+import os
+from distutils.core import setup
+
+scripts = [os.path.join('bin', f) for f in os.listdir('./bin')]
+packages = ['news_classifier']
+setup(name='', version='0.1', scripts=scripts, packages=packages)
