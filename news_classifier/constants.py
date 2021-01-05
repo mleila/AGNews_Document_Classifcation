c24fef557799165d9d3845f5b16d99e17a81db9d
Mohamed Leila <mohamed.laila@mail.mcgill.ca>
2021-01-05 11:29:57 -0500

    [NEW] constants module

---
+++ news_classifier/constants.py
@@ -0,0 +1,13 @@
+
+TRAIN = 'train'
+VALID = 'valid'
+TEST = 'test'
+
+DATASET = 'dataset'
+TITLE_COL = 'title'
+LABEL_COL = 'category'
+
+
+UNK_TOKEN = '<unk>'
+SOS_TOKEN = '<sos>'
+EOS_TOKEN = '<eos>'
